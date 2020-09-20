53b4a25be53b4065510735cf38d139ef9d2379c7
nishanthkorupolu <korupolu.nishanth@gmail.com>
2020-09-20 20:55:20 +0530

    Scoring Added

---
+++ Assets/PlayerScore.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject collectible;
+    public Text text;
+    private int score = 0;
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            PlayerPrefs.SetInt("AstroScore", 0);
+            Application.Quit();
+        }
+    }
+
+    private void setScore()
+    {
+        score = PlayerPrefs.GetInt("AstroScore");
+        text.text = score.ToString();
+    }
+
+    void Start()
+    { 
+        float spawnY = Random.Range
+            (Camera.main.ScreenToWorldPoint(new Vector2(0, 35)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height-35)).y);
+        float spawnX = Random.Range
+            (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+
+        Vector2 spawnPosition = new Vector2(spawnX, spawnY);
+        Instantiate(collectible, spawnPosition, Quaternion.identity);
+        setScore();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Collectible")
+        {
+            PlayerPrefs.SetInt("AstroScore", score + 1);
+            Destroy(collider.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/XclaimR/Scripts/EnemyScore.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class EnemyScore : MonoBehaviour
+{
+    
+    public Text text;
+    private int score = 0;
+    
+
+    void Start()
+    {
+        setScore();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape")) { 
+            PlayerPrefs.SetInt("AlienScore", 0);
+            Application.Quit();
+        }
+    }
+
+    private void setScore()
+    {
+        score = PlayerPrefs.GetInt("AlienScore");
+        text.text = score.ToString();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            PlayerPrefs.SetInt("AlienScore", score+1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
