09b322b77dada49e4ff4f0943af0900cbc6a66a6
nishanthkorupolu <korupolu.nishanth@gmail.com>
2020-09-25 12:32:26 +0530

    Changes

---
+++ Assets/CameraSize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera cameraOutput;
+
+    void Start()
+    {
+        Debug.Log("Pixel width :" + cameraOutput.pixelWidth + " Pixel height : " + cameraOutput.pixelHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
